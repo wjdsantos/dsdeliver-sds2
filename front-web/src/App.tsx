f8b6da3e3af7ba5fde792fb3954156217c09840e
wjdsantos <walter4asantos@gmail.com>
2021-01-06 18:28:19 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
