6ea65e625c3ce49f9ac69084b8547e80e7f22576
wjdsantos <walter4asantos@gmail.com>
2021-01-09 11:28:51 -0300

    Correcões para o deploy no Netlify

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { formatPrice } from "./helpers";
 import { Product } from "./types";
 
--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function StepsHeader() {
     return (
         <header className="orders-steps-container">
